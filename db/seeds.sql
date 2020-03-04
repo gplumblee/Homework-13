a50d4acfe01af4ae52013b8af047e8dae99b1ba2
Greg Plumblee <gplumblee@hotmail.com>
2020-03-03 21:42:35 -0800

    created all files necessary, though not working properly

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Austin's Hawaiian Burger");
+INSERT INTO burgers (burger_name) VALUES ("Jose's Fiesta Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ariel's Sicilian Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Joanne's Fish and Chips", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Brett's Chili Cheeseburger", true);
+INSERT INTO burgers (burger_name) VALUES ("Zane's Zesty Everything Burger");
